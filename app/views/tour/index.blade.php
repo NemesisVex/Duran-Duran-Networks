efbc44767e77574fc6b2df74086e8acbb671f3b1
Greg Bueno <greg@gregbueno.com>
2014-05-25 23:08:27 -0700

    Migrating to Bootstrap.

--- app/views/tour/index.blade.php
+++ app/views/tour/index.blade.php
@@ -11,49 +11,56 @@
 </script>
 
 @if (!empty($tours))
-<div class="span-22 append-bottom">
-	<ul id="mycarousel" class="jcarousel-skin-ddn span-22 append-bottom">
-		@foreach ($tours as $one_tour)
-		<li> <a href="/tour/{{ $one_tour->tour_id }}/">{{ $one_tour->tour_name }}</a><br/>({{ date('M Y', strtotime($one_tour->dates->min('date_tour_date'))) }}-{{ date('M Y', strtotime($one_tour->dates->max('date_tour_date'))) }})</li>
-		@endforeach
-	</ul>
+<div id="nav-tour" class="row">
+	<nav class="col-md-12">
+		<ul id="mycarousel" class="jcarousel-skin-ddn">
+			@foreach ($tours as $one_tour)
+			<li> <a href="/tour/{{ $one_tour->tour_id }}/">{{ $one_tour->tour_name }}</a><br/>({{ date('M Y', strtotime($one_tour->dates->min('date_tour_date'))) }}-{{ date('M Y', strtotime($one_tour->dates->max('date_tour_date'))) }})</li>
+			@endforeach
+		</ul>
+	</nav>
 </div>
 @endif
 
-<div class="span-10 append-1">
-
-@if (!empty($tour))
-<h2>{{ $tour->tour_name }}</h2>
-
-<div id="tour_dates">
-	<p>
-		@foreach ($tour->dates as $tour_date)
-		<script type="text/javascript">
-		$(document).ready(function () {
-			point = new GLatLng({{ $tour_date->geocode->geocode_lat }}, {{ $tour_date->geocode->geocode_lon }});
-			create_marker(point, {{ $tour_date->date_id }}, $('#point_{{ $tour_date->date_id }}')[0]);
-		});
-		</script>
-		<strong>{{ date('M d, Y', strtotime($tour_date->date_tour_date)) }}</strong>:
-		<a href="javascript:" id="point_{{ $tour_date->date_id }}">{{ $tour_date->geocode->geocode_location }}</a>,
-		{{ $tour_date->geocode->geocode_city }}@if (!empty($tour_date->geocode->geocode_state)), {{ $tour_date->geocode->geocode_state }}@endif, {{ $tour_date->geocode->country->country_name }}<br/>
-	@endforeach
-	</p>
+<div class="row">
+	<div class="col-md-12">
+		<div id="map_canvas"></div>
+	</div>
 </div>
 
-@endif
+<div class="row">
+	<div class="col-md-6">
 
-</div>
+		@if (!empty($tour))
+		<h2>{{ $tour->tour_name }}</h2>
 
-<div class="span-11 last">
-	<div id="map_canvas"></div>
+		<div id="tour_dates">
+			<p>
+				@foreach ($tour->dates as $tour_date)
+				<script type="text/javascript">
+				$(document).ready(function () {
+					point = new GLatLng({{ $tour_date->geocode->geocode_lat }}, {{ $tour_date->geocode->geocode_lon }});
+					create_marker(point, {{ $tour_date->date_id }}, $('#point_{{ $tour_date->date_id }}')[0]);
+				});
+				</script>
+				<strong>{{ date('M d, Y', strtotime($tour_date->date_tour_date)) }}</strong>:
+				<a href="javascript:" id="point_{{ $tour_date->date_id }}">{{ $tour_date->geocode->geocode_location }}</a>,
+				{{ $tour_date->geocode->geocode_city }}@if (!empty($tour_date->geocode->geocode_state)), {{ $tour_date->geocode->geocode_state }}@endif, {{ $tour_date->geocode->country->country_name }}<br/>
+			@endforeach
+			</p>
+		</div>
 
-	<h3>Notes</h3>
+		@endif
 
-	<ul>
-		<li> In the case of venues no longer available, only the city, state and country are plotted on the map if an address could not be located.</li>
-		<li> Tour information provided by the <a href="http://duranduran.wikia.com/wiki/Category:Timeline">Duran Duran Timeline</a>.</li>
-	</ul>
+	</div>
+	<div class="col-md-6">
+		<h3>Notes</h3>
 
+		<ul>
+			<li> In the case of venues no longer available, only the city, state and country are plotted on the map if an address could not be located.</li>
+			<li> Tour information provided by the <a href="http://duranduran.wikia.com/wiki/Category:Timeline">Duran Duran Timeline</a>.</li>
+		</ul>
+
+	</div>	
 </div>
 @stop
